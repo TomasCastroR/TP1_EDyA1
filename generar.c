394d7238ec4a30b856527340053541aa0c6538f4
blasbarbagelata <blasbarbagelata@gmail.com>
2020-04-22 17:50:50 -0300

    comentado

--- generar.c
+++ generar.c
@@ -6,6 +6,8 @@
 #define LARGO_BUFFER 100
 #define RANGO_EDAD 100
 
+/*cantidad_de_lineas: File* -> Int
+Recibe un archivo, devuelve la cantidad de lineas que posee el archivo*/
 int cantidad_de_lineas (FILE *archivo) {
   int lineas = 0;
   char buffer[LARGO_BUFFER], caracter;
@@ -17,6 +19,9 @@ int cantidad_de_lineas (FILE *archivo) {
   return lineas;
 }
 
+/*liberar_memoria: char**, int
+Recibe un arreglo de punteros char y su tamaño,
+libera los espacios de memoria de cada puntero en el arreglo*/
 void liberar_memoria (char **arreglo, int largo) {
   for (int i = 0; i < largo; ++i) {
     free (arreglo[i]);
@@ -24,6 +29,10 @@ void liberar_memoria (char **arreglo, int largo) {
   free (arreglo);
 }
 
+/* crear_arreglo_localidades: FILE* , int , char**
+Recibe un archivo de localidades, la cantidad de lineas del archivo
+y un arreglo de punteros char. 
+Le asigna a cada puntero char una localidad del archivo */
 void crear_arreglo_localidades (FILE *archivo, int largo, char **localidades) {
   char buffer[LARGO_BUFFER];
   for (int i = 0; i < largo; ++i) {
@@ -33,7 +42,11 @@ void crear_arreglo_localidades (FILE *archivo, int largo, char **localidades) {
     localidades[i][strlen(buffer) + 1] = '\0';
   }
 }
-
+/*arreglo_num_random: int*, int, int
+Recibe un arreglo de punteros int, la cantidad de personas deseadas y el
+total de personas del archivo.
+ Crea un arreglo (de tamaño cantPersonas) de numeros aleatorios
+ de 0 a TotalPersonas-1 sin repetir*/
 void arreglo_num_random (int *arregloRandom, int cantPersonas, int totalPersonas) {
   int *arregloIndex = malloc (sizeof(int) * totalPersonas);
   int numeroRandom, inicializador = 0, marcador = -1;
@@ -63,8 +76,13 @@ void arreglo_num_random (int *arregloRandom, int cantPersonas, int totalPersonas
   }
   free (arregloIndex);
 }
-
-void lectura_escritura (int cantPersonas, int arregloRandom[],
+/* lectura_escritura: int, int *, char*, char*, char* 
+  Recibe la cantidad de personas, el arreglo de numeros random, el nombre 
+  del archivo de personas, el nombres del archivo de paises, el nombre del archivo
+  que devuelve.
+    Escribe sobre un archivo las personas, con una edad aleatoria que va del 1 al
+  100 y una localidad aleatoria del archivo de localidades*/
+void lectura_escritura (int cantPersonas, int *arregloRandom,
                 char *archivoNombres, char *archivoPaises, char *archivoProg1) {
   FILE *archivoPersonas, *archivoLocalidades, *archivoSalida;
   char buffer[LARGO_BUFFER], nombreApellido[LARGO_BUFFER];
